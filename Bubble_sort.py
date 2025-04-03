c82c13d3f172dca4a106e2c95f023600f96e26b8
Nguyen Cong Hieu <136480880+KFdc15@users.noreply.github.com>
2025-04-03 16:53:55 +0700

    Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,4 +1,11 @@
 # Sắp xếp mảng bằng thuật toán Bubble Sort
+n = int(input("Nhập số lượng phần tử của mảng: "))
+arr = []
+print("Nhập các phần tử của mảng:")
+for i in range(n):
+    arr.append(int(input()))    
+print("Mảng trước khi sắp xếp:")
+print(arr)
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
@@ -6,3 +13,5 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+print("Mảng sau khi sắp xếp:")
+print(bubble_sort(arr))
